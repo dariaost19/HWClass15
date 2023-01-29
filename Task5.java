7da685e13c6b5c050bea1c81f7a6aa2b40b6c1a1
dariaost19 <dariaost19@gmail.com>
2023-01-28 22:02:01 -0500

    udatestoTask5

--- Task5.java
+++ Task5.java
@@ -6,17 +6,19 @@ public class Task5 {
 // Prime number is a whole number greater than 1 that cannot be exactly divided by any whole number other than itself and 1
     Boolean number(int num) {
         boolean prime = true;
-        for (int i = 2; i <= num; i++) {
-            if (num % i != 0) {
-                return true;
-            } else {
-                return false;
+        if (num > 1) {
+            for (int i = 2; i <= num / 2; i++) {
+                if (num % i == 0) {
+                    return false;
+                }
             }
 
         }
         return prime;
     }
 }
+
+
 
 
 
