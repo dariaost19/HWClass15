09b019f5ef2b32f858dedb931d174b80e8d356a4
dariaost19 <dariaost19@gmail.com>
2023-01-28 21:19:52 -0500

    udatedHW

--- Task5.java
+++ Task5.java
@@ -2,21 +2,44 @@ package Homework12;
 
 public class Task5 {
     //5) Write a method to return whether given number is prime or not?
+//What is a prime number in math?
+// Prime number is a whole number greater than 1 that cannot be exactly divided by any whole number other than itself and 1
     Boolean number(int num) {
-        boolean prime = true;
-        for (int i = 2; i <= num / 2; i++) {
-
-        }
-        if (num % 2 != 0) {
+        if (num > 1) {
+            for (int i = 2; i < num; i++) {
+                if (num % i != 0) {
+                    return false;
+                }
+            }
+                } else {
+                    return false;
+                }
             return true;
 
-        } else {
-            return false;
         }
     }
 
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- Task5Tester.java
+++ Task5Tester.java
@@ -3,6 +3,6 @@ package Homework12;
 public class Task5Tester {
     public static void main(String[] args) {
         Task5 tester=new Task5();
-        System.out.println(tester.number(11));
+        System.out.println(tester.number(20));
     }
 }
