91a960ad0948bbe1e2f8a29b9d96b8e4ddf2c578
dariaost19 <dariaost19@gmail.com>
2023-01-28 21:48:59 -0500

    updatedtask5

--- Task5.java
+++ Task5.java
@@ -5,19 +5,27 @@ public class Task5 {
 //What is a prime number in math?
 // Prime number is a whole number greater than 1 that cannot be exactly divided by any whole number other than itself and 1
     Boolean number(int num) {
-        if (num > 1) {
-            for (int i = 2; i < num; i++) {
-                if (num % i != 0) {
-                    return false;
-                }
+        boolean prime = true;
+        for (int i = 2; i <= num; i++) {
+            if (num % i != 0) {
+                return true;
+            } else {
+                return false;
             }
-                } else {
-                    return false;
-                }
-            return true;
 
         }
+        return prime;
     }
+}
+
+
+
+
+
+
+
+
+
 
 
 
